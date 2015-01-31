dbaca894230efc2e35cd2af576f99d832f2147ec
Joel Frederico <joelfred@slac.stanford.edu>
2015-01-30 17:55:17 -0800

    Full path fixes, formatting, floats

--- elegant_sim.py
+++ elegant_sim.py
@@ -14,7 +14,7 @@ loggerlevel = logging.DEBUG
 
 __all__ = ['elegant_sim']
 
-def elegant_sim(beamline,dir=None):
+def elegant_sim(beamline,dir=None,**kwargs):
     # ======================================
     # If path isn't specified, create
     # temporary one
@@ -33,22 +33,38 @@ def elegant_sim(beamline,dir=None):
     logger.log(level=loggerlevel,msg='Filename is: {}'.format(filename))
 
     basename = os.path.basename(filename)
-    lte_name = '{}.lte'.format(basename)
+    root,ext = os.path.splitext(basename)
+    lte_name = os.path.join(path,'{}.lte'.format(root))
     beamline.elegant_lte(filename=lte_name)
     
+    # ======================================
+    # Get template path from package and
+    # load template
+    # ======================================
     templates_path = pkg_resources.resource_filename(__name__,'templates')
-    ipdb.set_trace()
     loader = jj.FileSystemLoader(templates_path)
     env = jj.Environment(loader=loader)
-    
     template = env.get_template('template.ele')
     
-    template.stream(filename=lte_name,matched=0).dump(f)
+    # ======================================
+    # Generate ele file
+    # ======================================
+    template.stream(filename=lte_name,matched=0,**kwargs).dump(f)
+    f.close()
 
+    # ======================================
+    # Run Elegant
+    # ======================================
     command = 'elegant {}'.format(filename)
     logger.log(level=loggerlevel,msg='Command is: {}'.format(command))
-    
-    subprocess.call(shlex.split(command))
 
+    fnull = open(os.devnull,'w')
+    subprocess.call(shlex.split(command),stdout=fnull,stderr=fnull)
+
+    # ======================================
+    # Clean temporary directory if necessary
+    # ======================================
     if dir is None:
         shutil.rmtree(path)
+
+    return path,root,ext
