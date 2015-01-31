8e5d216e94f1904022f29597c295911f456d31db
Joel Frederico <joelfred@slac.stanford.edu>
2015-01-30 17:54:10 -0800

    Conversions now handles lists

--- conversions.py
+++ conversions.py
@@ -1,7 +1,16 @@
-electron_mc2_gev = 0.510998910e-3
+import numpy as _np
+electron_mc2_gev = _np.float_(0.510998910e-3)
 
 def gamma2GeV(gamma):
-    return gamma*electron_mc2_gev
+    n_pts = _np.size(gamma)
+    if n_pts > 1:
+        for i,val in enumerate(gamma):
+            gamma[i] = gamma2GeV(val)
+        return gamma
+    else:
+        gamma = _np.float(gamma)
+        return gamma*electron_mc2_gev
 
 def GeV2gamma(E):
+    E = _np.float(E)
     return E/electron_mc2_gev
