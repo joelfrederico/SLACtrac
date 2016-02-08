f66ef2718f0571b1ef991936873351d7435de1a9
Joel Frederico <joelfred@slac.stanford.edu>
2016-02-07 19:51:14 -0800

    More tests

--- tests/base.py
+++ tests/base.py
@@ -1,9 +1,27 @@
 import unittest
+import numpy as np
 
 
 class slactrac_element_base(object):
+    def basic_setup(self, length, order, name):
+        self._l       = length
+        self._order   = order
+        self._name    = name
+
+    @property
+    def element(self):
+        return self._element
+
+    @element.setter
+    def element(self, val):
+        self._element = val
+
     def name_test(self):
         self.assertEqual(self.element.name, self._name)
 
     def order_test(self):
         self.assertEqual(self.element.order, self._order)
+
+
+def array_tol(meas, ref, tol):
+    return np.all(meas - ref < tol)
