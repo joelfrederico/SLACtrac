c76f719887d97894148301e123d64917b2770de2
Joel Frederico <joelfred@slac.stanford.edu>
2016-02-08 17:34:11 -0800

    More verbose testing

--- tests/conversion_tests.py
+++ tests/conversion_tests.py
@@ -8,21 +8,31 @@ gamma = 39824
 class conversion_tests(unittest.TestCase):
     def GeV2gamma_test(self):
         gamma = st.GeV2gamma(E)
-        self.assertEqual(gamma, 39823.95833089439)
+        test  = 39823.95833089439
+        delta = 0
+        print('Evaluated: {}'.format(gamma))
+        print('Delta: {}'.format(gamma-test))
+        self.assertAlmostEqual(gamma, test, delta)
 
     def GeV2joule_test(self):
         joule = st.GeV2joule(E)
-        self.assertEqual(joule, 3.2604293097750004e-09)
+        test  = 3.2604293097750004e-09
+        delta = 0
+        print('Evaluated: {}'.format(joule))
+        print('Delta: {}'.format(joule-test))
+        self.assertAlmostEqual(joule, test, delta)
 
     def eV2joule_test(self):
         joule = st.eV2joule(E*1e9)
         test = 3.2604293097750002e-09
-        print(joule-test)
+        print('Evaluated: {}'.format(joule))
+        print('Delta: {}'.format(joule-test))
         self.assertAlmostEqual(joule, test, delta=1e-24)
 
     def gamma2GeV_test(self):
         En    = st.eV2joule(gamma)
         test  = 6.3805079524560005e-15
         delta = 0
-        print(En-test)
+        print('Evaluated: {}'.format(En))
+        print('Delta: {}'.format(En-test))
         self.assertAlmostEqual(En, test, delta=delta)
